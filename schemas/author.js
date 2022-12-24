bbed3b3044975ea1b5e4b34b23e40f9756d2cbc4
Canal do Front <114775567+canaldofront@users.noreply.github.com>
2022-12-24 12:27:46 -0300

    Add files via upload

---
+++ schemas/author.js
@@ -0,0 +1,22 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'author',
+  title: 'Author',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
---
+++ schemas/index.js
@@ -0,0 +1,5 @@
+import blockContent from './blockContent'
+import post from './post'
+import author from './author'
+
+export const schemaTypes = [post, author, blockContent]
